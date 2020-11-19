a635cf796155392bf9ef2ce6abd4db2f387f89a3
Xalizar6 <dave.rukamp@gmail.com>
2020-11-18 23:44:58 -0600

    'test' task not loaded so removing, and formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,52 @@
-let matchdep = require( 'matchdep' );
-let mergeFiles = require( './grunt-scripts/mergeFiles' );
+let matchdep = require('matchdep');
+let mergeFiles = require('./grunt-scripts/mergeFiles');
 
-module.exports = function ( grunt ) {
-    // Add stats for time spent on each task
-    require( 'time-grunt' )( grunt );
+module.exports = function (grunt) {
+  // Add stats for time spent on each task
+  require('time-grunt')(grunt);
 
-    // Pull defaults (including username and password) from .screeps.json
-    const config = require( './.screeps.json' )
+  // Pull defaults (including username and password) from .screeps.json
+  const config = require('./.screeps.json')
 
-    // Allow grunt options to override default configuration
-    const email = grunt.option( 'email' ) || config.email;
-    const password = grunt.option( 'password' ) || config.password;
-    const branch = grunt.option( 'branch' ) || config.branch;
-    const ptr = grunt.option( 'ptr' ) ? true : config.ptr
+  // Allow grunt options to override default configuration
+  const email = grunt.option('email') || config.email;
+  const password = grunt.option('password') || config.password;
+  const branch = grunt.option('branch') || config.branch;
+  const ptr = grunt.option('ptr') ? true : config.ptr
 
-    // Load needed tasks
-    matchdep.filterAll( [ 'grunt-*', '!grunt-cli' ] ).forEach( grunt.loadNpmTasks );
+  // Load needed tasks
+  matchdep.filterAll(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
 
-    mergeFiles( grunt );
+  mergeFiles(grunt);
 
-    grunt.initConfig( {
-        screeps: {
-            options: {
-                email: email,
-                password: password,
-                branch: branch,
-                ptr: ptr,
-            },
-            dist: {
-                src: [ 'dist/*.js' ]
-            }
-        },
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: email,
+        password: password,
+        branch: branch,
+        ptr: ptr,
+      },
+      dist: {
+        src: ['dist/*.js']
+      }
+    },
 
-        copy: {
-            main: {
-                expand: true,
-                flatten: true,
-                filter: 'isFile',
-                cwd: 'dist/',
-                src: '**',
-                dest: 'Update This Path'
-            },
-        },
-    } );
+    copy: {
+      main: {
+        expand: true,
+        flatten: true,
+        filter: 'isFile',
+        cwd: 'dist/',
+        src: '**',
+        dest: 'Update This Path'
+      },
+    },
+  });
 
-    grunt.registerTask( 'main', [ 'test', 'merge', 'write' ] );
-    grunt.registerTask( 'sandbox', [ 'merge', 'write-private' ] );
-    grunt.registerTask( 'merge', 'mergeFiles' );
-    grunt.registerTask( 'write', 'screeps' );
-    grunt.registerTask( 'write-private', 'copy' );
+  grunt.registerTask('main', ['merge', 'write']);
+  grunt.registerTask('sandbox', ['merge', 'write-private']);
+  grunt.registerTask('merge', 'mergeFiles');
+  grunt.registerTask('write', 'screeps');
+  grunt.registerTask('write-private', 'copy');
 };
